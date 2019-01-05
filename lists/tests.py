b14fcccee8705b637ab00da71109b9154070e006
Tur8008 <art8008@gmail.com>
2019-01-05 23:17:55 +0300

    lists app files added

---
+++ lists/tests.py
@@ -0,0 +1,12 @@
+from django.urls import resolve
+from django.test import TestCase
+
+from .views import home_page
+
+# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        return self.assertEqual(found.func, home_page)
