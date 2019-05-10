49457912ef0f15669118a63971fcc0c5044643c2
Artur Ivanov <art8008@gmail.com>
2019-05-10 21:20:00 +0300

    adjust test to it be able to create session against stagin server

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,4 +1,7 @@
 import re
+import time
+import poplib
+import os
 
 from django.core import mail
 
@@ -6,18 +9,57 @@ from selenium.webdriver.common.keys import Keys
 
 from .base import FunctionalTest
 
-TEST_EMAIL = 'edith@example.com'
+# TEST_EMAIL = 'edith@example.com'
 SUBJECT = 'Your login link for Superlists'
 
 
 class LoginTest(FunctionalTest):
 
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertIn(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop.gmail.com')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['RECEIVER_PASSWORD'])
+            while time.time() - start < 60:
+                # get 10 newest messages
+                count, _ = inbox.stat()
+                print('number of emails:', len(inbox.list()[1]))
+                print(count)
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting msg', i)
+                    _, lines, __ = inbox.retr(i)
+                    # print(len(lines))
+                    lines = [l.decode('utf8') for l in lines]
+                    # print(lines)
+                    if f'Subject: {subject}' in lines:
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+                time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit
+
     def test_can_get_email_link_to_log_in(self):
         # Edith goes to the awesome superlists site
         # and notices a "Log in" section in the navbar for the first time
         # It's telling her to enter her email address, so she does
+        if self.staging_server:
+            test_email = 'art8008@gmail.com'
+        else:
+            test_email = 'edith@example.com'
+
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(test_email)
         self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
 
         # A message appears telling her an email has been sent
@@ -27,15 +69,16 @@ class LoginTest(FunctionalTest):
         ))
 
         # She checks her email and finds a message
-        email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
-
+        # email = mail.outbox[0]
+        # self.assertIn(TEST_EMAIL, email.to)
+        # self.assertEqual(email.subject, SUBJECT)
+        body = self.wait_for_email(test_email, SUBJECT)
+        print(type(body))
         # It has a url link in it
-        self.assertIn('Use this link to login', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIn('Use this link to login', body)
+        url_search = re.search(r'http://.+/.+$', body)
         if not url_search:
-            self.fail(f'Could not find url in email body:\n{email.body}')
+            self.fail(f'Could not find url in email body:\n{body}')
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
 
@@ -43,10 +86,10 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
 
         # she is logged in!
-        self.wait_to_be_logged_in(email=TEST_EMAIL)
+        self.wait_to_be_logged_in(email=test_email)
 
         # Now she logs out
         self.browser.find_element_by_link_text('Log out').click()
 
         # She is logged out
-        self.wait_to_be_logged_out(email=TEST_EMAIL)
+        self.wait_to_be_logged_out(email=test_email)
